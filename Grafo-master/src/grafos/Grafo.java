094763a56a19f9f1710ecd63784e16bd97dd1b14
joxelito950 <jose-950@hotmail.com>
2018-10-01 16:16:56 -0500

    joxelito950
    se arregla nombramiento y se limpia proyecto para volver a iniciar

---
+++ Grafo-master/src/grafos/Grafo.java
@@ -0,0 +1,25 @@
+
+package grafos;
+
+public class Grafo {
+
+	Grafo() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public void insertar(char datoObtenido) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void dijkstra(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void eliminar(char datoObtenido) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- Grafo-master/src/grafos/Grafos.java
+++ Grafo-master/src/grafos/Grafos.java
@@ -1,25 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package grafos;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author sala203
- */
 public class Grafos {
 
-    /**
-     * @param args the command line arguments
-     */
+	private static Grafo grafo =new Grafo();
+	
     public static void main(String[] args) {
-       Dijkstra Grafo;
-        Grafo = new Dijkstra();
-        String menu = "**MENU**" + "\n" + "0. Salir" + "\n" + "11. Mostrar" + "\n" + "22. Insertar" + "\n" + "33. Eliminar." + " \n + 44. Buscar";
+        String menu = "0. Salir" + "\n" + "11. Mostrar" + "\n" + "22. Insertar" + "\n" + "33. Eliminar." + " \n + 44. Buscar";
         char datoObtenido;
         while (true) {
             int opcionSelecionadaDelMenu = Integer.parseInt(JOptionPane.showInputDialog(null, menu, "MENU", JOptionPane.DEFAULT_OPTION));
@@ -27,21 +16,17 @@ public class Grafos {
                 case 0:
                     return;
                 case 11:
-                    System.out.println("Estos son los datos del arbol: ");
-                    Grafo.mostrar(Graf.retornarRaiz());
-                    System.out.println("Ya estan todos los datos.");
+                    grafo.dijkstra(0);
                     break;
                 case 22:
                     String datoAInsertar = JOptionPane.showInputDialog(null, "Debe ingresar un solo caracter", "Ingrese Dato", JOptionPane.DEFAULT_OPTION);
                     datoObtenido = datoAInsertar.charAt(0);
-                    Grafo.InsertarNodo();
-                    System.out.println("Dato Insertado");
+                    grafo.insertar(datoObtenido);
                     break;
                 case 33:
                     String datoAEliminar = JOptionPane.showInputDialog(null, "Debe ingresar el caracter a eliminar", "Eliminar Caracter", JOptionPane.DEFAULT_OPTION);
                     datoObtenido = datoAEliminar.charAt(0);
-                    Grafo.eliminarNodo(datoObtenido);
-                    System.out.println("Dato Eliminado");
+                    grafo.eliminar(datoObtenido);
                     break;
                 default:
                     JOptionPane.showMessageDialog(null, "Opcion no valida", "Intente de nuevo", JOptionPane.WARNING_MESSAGE);
